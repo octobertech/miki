132baee886743e10ab52e7cb6146a31ca93865a1
Atai Konushaliev <octobrtech@gmail.com>
2014-12-26 22:16:23 +0600

    Deleted excess serializers.py file.

--- main/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from main.models import Link
-
-class LinkSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=50)
-    body = serializers.
